4fbedf6f8821012c7f538de37ecd87a015ed2e2f
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-10-27 13:16:49 +0100

    ddddd

--- Sources/P42-screenelements/screenelements.swift
+++ Sources/P42-screenelements/screenelements.swift
@@ -3,17 +3,17 @@
 import SwiftUI
 
 @available(iOS 13.0, *)
-struct VScrollView<Content>: View where Content: View {
-  @ViewBuilder let content: Content
-  
+public struct VScrollView<Content>: View where Content: View {
+    @ViewBuilder let content: Content
+    
     @available(iOS 13.0.0, *)
-    var body: some View {
-    GeometryReader { geometry in
-      ScrollView(.vertical) {
-        content
-          .frame(width: geometry.size.width)
-          .frame(minHeight: geometry.size.height)
-      }
+    public var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                content
+                    .frame(width: geometry.size.width)
+                    .frame(minHeight: geometry.size.height)
+            }
+        }
     }
-  }
 }
