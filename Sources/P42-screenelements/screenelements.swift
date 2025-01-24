adcffaeafbeedb7b7a6d88815183630af205b072
Diederick de Buck <diederick.de.buck@platform-42.com>
2025-01-24 16:34:51 +0100

    added content header

--- Sources/P42-screenelements/screenelements.swift
+++ Sources/P42-screenelements/screenelements.swift
@@ -5,6 +5,60 @@ import SwiftUI
 
 import SwiftUI
 
+
+@available(iOS 13.0, *)
+public struct ContentHeader: View {
+    public var titleLabel: String
+    public var logo: String
+    public var logoColor: Color
+    public var portraitSize: CGFloat
+    public var info: String? = nil
+    public var width: CGFloat
+
+    public init(
+        titleLabel: String,
+        logo: String,
+        logoColor: Color = .primary,
+        portraitSize: CGFloat = 100,
+        info: String? = nil,
+        width: CGFloat = UIScreen.main.bounds.width * 0.8
+    ) {
+        self.titleLabel = titleLabel
+        self.logo = logo
+        self.logoColor = logoColor
+        self.portraitSize = portraitSize
+        self.info = info
+        self.width = width
+    }
+
+    public var body: some View {
+        VStack(spacing: 16) {
+            Text(titleLabel)
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 8)
+
+            Image(systemName: logo)
+                .resizable()
+                .scaledToFit()
+                .frame(width: portraitSize, height: portraitSize)
+                .foregroundColor(logoColor)
+
+            if let info = info {
+                Text(info)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .padding(.top, 8)
+            }
+        }
+        .frame(width: width)
+        .padding()
+    }
+}
+
+
 @available(iOS 13.0, *)
 public struct VScrollView<Content: View>: View {
     private let axis: Axis.Set
