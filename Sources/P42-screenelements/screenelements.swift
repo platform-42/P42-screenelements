882f04b596a5ab260e3989aabfba62ee656e10b4
Diederick de Buck <diederick.de.buck@platform-42.com>
2025-04-16 19:01:38 +0200

    ddddd

--- Sources/P42-screenelements/screenelements.swift
+++ Sources/P42-screenelements/screenelements.swift
@@ -7,8 +7,6 @@ import SwiftUI
 
 
 @available(iOS 13.0, *)
-
-
 public struct ContentHeader: View {
     public var titleLabel: String
     public var logo: String
@@ -17,7 +15,7 @@ public struct ContentHeader: View {
     public var info: String? = nil
     public var width: CGFloat
     public var onLogoTap: (() -> Void)? = nil
-
+    
     public init(
         titleLabel: String,
         logo: String,
@@ -35,10 +33,14 @@ public struct ContentHeader: View {
         self.width = width
         self.onLogoTap = onLogoTap
     }
-
+    
     public var body: some View {
-        VStack(spacing: 8) {
-            // Logo
+        VStack(spacing: 10) {
+            Text(titleLabel)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 5)
+            
             Image(systemName: logo)
                 .resizable()
                 .scaledToFit()
@@ -47,62 +49,6 @@ public struct ContentHeader: View {
                 .onTapGesture {
                     onLogoTap?()
                 }
-
-            // Title
-            Text(titleLabel)
-                .font(.title)
-                .bold()
-
-            // Optional info
-            if let info = info {
-                Text(info)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
-        }
-        .frame(width: width)
-        .padding()
-    }
-}
-
-/*
-public struct ContentHeader: View {
-    public var titleLabel: String
-    public var logo: String
-    public var logoColor: Color
-    public var portraitSize: CGFloat
-    public var info: String? = nil
-    public var width: CGFloat
-
-    public init(
-        titleLabel: String,
-        logo: String,
-        logoColor: Color = .primary,
-        portraitSize: CGFloat = 100,
-        info: String? = nil,
-        width: CGFloat = UIScreen.main.bounds.width * 0.8
-    ) {
-        self.titleLabel = titleLabel
-        self.logo = logo
-        self.logoColor = logoColor
-        self.portraitSize = portraitSize
-        self.info = info
-        self.width = width
-    }
-
-    public var body: some View {
-        VStack(spacing: 10) {
-            Text(titleLabel)
-                .font(.title)
-                .fontWeight(.bold)
-                .padding(.bottom, 5)
-
-            Image(systemName: logo)
-                .resizable()
-                .scaledToFit()
-                .frame(width: portraitSize, height: portraitSize)
-                .foregroundColor(logoColor)
-
             if let info = info {
                 Text(info)
                     .font(.body)
@@ -113,7 +59,7 @@ public struct ContentHeader: View {
         .padding()
     }
 }
-*/
+
 
 @available(iOS 13.0, *)
 public struct VScrollView<Content: View>: View {
