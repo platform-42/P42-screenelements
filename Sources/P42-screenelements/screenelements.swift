f55b66040ac30d1d0412b76bb3d9ffaec6ec5dd5
Diederick <diederick.de.buck@platform-42.com>
2026-02-14 20:09:39 +0100

    dddd

--- Sources/P42-screenelements/screenelements.swift
+++ Sources/P42-screenelements/screenelements.swift
@@ -3,12 +3,6 @@
 import SwiftUI
 
 
-public enum BadgedLabelContent {
-    case text(String)
-    case systemImage(name: String)
-}
-
-
 @available(macOS 10.15, *)
 @available(iOS 13.0, *)
 public struct ContentHeader: View {
@@ -180,49 +174,3 @@ public struct StyledGroupBox<Content: View>: View {
     }
 }
 
-
-@available(iOS 14.0, *)
-public struct BadgedLabel: View {
-    
-    let content: BadgedLabelContent
-    let foregroundColor: Color
-    let font: Font
-    let backgroundColor: Color
-    let padding: EdgeInsets
-    
-    public init(
-        content: BadgedLabelContent,
-        foregroundColor: Color = .white,
-        font: Font = .caption2,
-        backgroundColor: Color = .blue,
-        padding: EdgeInsets = EdgeInsets(top: 2, leading: 6, bottom: 2, trailing: 6)
-    ) {
-        self.content = content
-        self.foregroundColor = foregroundColor
-        self.font = font
-        self.backgroundColor = backgroundColor
-        self.padding = padding
-    }
-    
-    public var body: some View {
-        label
-            .padding(padding)
-            .background(backgroundColor)
-            .clipShape(Capsule())
-    }
-    
-    @ViewBuilder
-    private var label: some View {
-        switch content {
-        case .text(let value):
-            Text(value)
-                .font(font)
-                .foregroundColor(foregroundColor)
-            
-        case .systemImage(let name):
-            Image(systemName: name)
-                .font(font)
-                .foregroundColor(foregroundColor)
-        }
-    }
-}
